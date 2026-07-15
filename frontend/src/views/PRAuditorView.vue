930f16f6ee1a6e0f112fdb76648134126d94f33e
MilneB21 <ben21jaminlim@gmail.com>
2026-07-15 15:29:14 +0800

    fix responsive workbench result panels

--- frontend/src/views/PRAuditorView.vue
+++ frontend/src/views/PRAuditorView.vue
@@ -139,15 +139,23 @@
           <div v-if="visibleActiveSessionJobs.length === 0" class="cockpit-empty-card">
             No active jobs are running or queued in this browser tab.
           </div>
-          <div v-else class="download-compact">
+          <div v-else class="download-compact active-jobs-scroll">
             <table class="active-jobs-table">
+              <colgroup>
+                <col class="active-job-col-id" />
+                <col class="active-job-col-worker" />
+                <col class="active-job-col-status" />
+                <col class="active-job-col-created" />
+                <col class="active-job-col-view" />
+                <col class="active-job-col-cancel" />
+              </colgroup>
               <thead>
                 <tr>
                   <th>Job ID</th>
                   <th>Worker</th>
                   <th>Status</th>
                   <th>Created</th>
-                  <th>View</th>
+                  <th>Live Output</th>
                   <th>Stop/Cancel</th>
                 </tr>
               </thead>
@@ -160,16 +168,16 @@
                   <td>{{ job.jobId }}</td>
                   <td>{{ job.workerDisplayName || job.workerId }}</td>
                   <td>{{ job.status }}</td>
-                  <td>{{ job.createdAt || 'Now' }}</td>
+                  <td>{{ formatDateTime(job.createdAt) }}</td>
                   <td>
-                    <button type="button" class="secondary-link" @click="selectActiveJob(job.jobId)">
-                      View
+                    <button type="button" class="secondary-link active-job-action" @click="selectActiveJob(job.jobId)">
+                      View Live Output
                     </button>
                   </td>
                   <td>
                     <button
                       type="button"
-                      class="secondary-link"
+                      class="secondary-link active-job-action"
                       :disabled="!isJobCancellable(job)"
                       @click="prepareCancellationForJob(job.jobId)"
                     >
